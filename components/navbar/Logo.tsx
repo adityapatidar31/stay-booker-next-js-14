826c2e24a3f57e380cdb628af279a1b8c33d1471
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-04 19:34:52 +0530

    create logo with the help of react icon

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { FaStaylinked } from "react-icons/fa";
+import { Button } from "../ui/button";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <FaStaylinked className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;

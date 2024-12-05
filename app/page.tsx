c4fbb077dc68c989dee820499d5ef8b8ee529d36
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-05 23:50:38 +0530

    add Categories LIst and PRoperties Container on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-function HomePage() {
-  return <div>Home Page</div>;
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
+
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
+  return (
+    <section>
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      <PropertiesContainer
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+    </section>
+  );
 }
 
 export default HomePage;

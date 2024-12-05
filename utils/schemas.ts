b316e64bd9b379ffbf3cda51a8ed7764b263ddec
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-05 19:04:55 +0530

    change contain of the message(error)

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -3,12 +3,12 @@ import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
   firstName: z.string().min(2, {
-    message: "first name must be at least 2 characters",
+    message: "First name must be at least 2 characters",
   }),
   lastName: z.string().min(2, {
-    message: "last name must be at least 2 characters",
+    message: "Last name must be at least 2 characters",
   }),
   username: z.string().min(2, {
-    message: "user name must be at least 2 characters",
+    message: "Username must be at least 2 characters",
   }),
 });

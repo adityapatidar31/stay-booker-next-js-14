14d599fa1c66c7a21ea9ff8a6010a4e021d5b0ef
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-06 21:10:20 +0530

    add BreadCrumbs in the propperty page and also basic stufs

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,24 @@
-function PropertyDetailPage() {
-  return <div>PropertyDetailPage</div>;
-}
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import { fetchPropertyDetails } from "@/utils/actions";
+import { redirect } from "next/navigation";
 
-export default PropertyDetailPage;
+async function PropertyDetailsPage({ params }: { params: { id: string } }) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect("/");
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold ">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/* share button */}
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
+}
+export default PropertyDetailsPage;

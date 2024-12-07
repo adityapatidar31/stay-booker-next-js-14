e54b498af2e7daa2d9709cd93ba294e9d40d2f86
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-07 23:18:39 +0530

    modify the fetchPropertyDetails add booking things also like chechIn and checkOut

--- utils/actions.ts
+++ utils/actions.ts
@@ -263,6 +263,12 @@ export const fetchPropertyDetails = (id: string) => {
     },
     include: {
       profile: true,
+      bookings: {
+        select: {
+          checkIn: true,
+          checkOut: true,
+        },
+      },
     },
   });
 };

f1161b3d14b7dde22fd8873de3de120be20553bf
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-09 19:23:41 +0530

    add loading the admin page with the help of suspense

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,18 @@
-function AdminPage() {
-  return <div>AdminPage</div>;
+import {
+  ChartsLoadingContainer,
+  StatsLoadingContainer,
+} from "@/components/admin/Loading";
+import { Suspense } from "react";
+async function AdminPage() {
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <div>Hey</div>
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <div>Hey</div>
+      </Suspense>
+    </>
+  );
 }
-
 export default AdminPage;

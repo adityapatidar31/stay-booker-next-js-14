0569ba14a1cf35ef85d1ba08f53755a7bc48e9b9
Aditya Patidar <adityapatidar.2020@gmail.com>
2024-12-09 19:01:06 +0530

    Create a fetchReservations server action for fetching all the booking for the property owner

--- utils/actions.ts
+++ utils/actions.ts
@@ -596,3 +596,31 @@ export const updatePropertyImageAction = async (
     return renderError(error);
   }
 };
+
+export const fetchReservations = async () => {
+  const user = await getAuthUser();
+
+  const reservations = await db.booking.findMany({
+    where: {
+      property: {
+        profileId: user.id,
+      },
+    },
+
+    orderBy: {
+      createdAt: "desc",
+    },
+    // include property details in the result
+    include: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          price: true,
+          country: true,
+        },
+      },
+    },
+  });
+  return reservations;
+};
